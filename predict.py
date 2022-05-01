08fc553f2a7483bcffaf06a456c3c58269dbca80
Lunara07 <49828654+Lunara07@users.noreply.github.com>
2022-05-01 22:42:30 +0300

    sd

--- predict.py
+++ predict.py
@@ -7,18 +7,8 @@ import pandas as pd
 from utils.image import predict_image, process_image
 from utils.model import load_model
 import pickle
-from tensorflow import keras
-import numpy as np
-from sklearn.preprocessing import StandardScaler
 model = None
 
-def create_dataset(X, y, time_steps=1):
-    Xs, ys = [], []
-    for i in range(len(X) - time_steps):
-        v = X.iloc[i:(i + time_steps)].values
-        Xs.append(v)        
-        ys.append(y.iloc[i + time_steps])
-    return np.array(Xs), np.array(ys)
 
 def predict(environ, start_response):
     # Load inputdata from the HTTP request
@@ -27,21 +17,10 @@ def predict(environ, start_response):
     if not request.files:
         return Response('no file uploaded', 400)(environ, start_response)
     csv_file = next(request.files.values())
-    df = pd.read_csv(csv_file)
-    train_size = int(len(df) * 0.95)
-    test_size = len(df) - train_size
-    train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
-    scaler = StandardScaler()
-    scaler = scaler.fit(train[['Delay']])
-    TIME_STEPS = 30
-
-    # reshape to [samples, time_steps, n_features]
-    
-    X_test, y_test = create_dataset(test[['Delay']], test.Delay, TIME_STEPS)
-
-    
-
-    
+    test = pd.read_csv(csv_file)
+    one_hot_encoded_data2 = pd.get_dummies(test, columns = ['Code'])
+    df2 = one_hot_encoded_data2[["Delay", "Code_200", "Code_201", "Code_204", "Code_302", "Code_400", "Code_404", "Code_500"]]
+    #image, inverted = process_image(Image.open(image_file))
 
     # The predictor must be lazily instantiated;
     # the TensorFlow graph can apparently not be shared
@@ -50,24 +29,11 @@ def predict(environ, start_response):
     if not model:
         with open('model.h5', 'rb') as f:
             model = pickle.load(f)
-
-    X_test_pred = model.predict(X_test)
-
-    test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
-    THRESHOLD = 0.65
-
-    test_score_df = pd.DataFrame(index=test[TIME_STEPS:].index)
-    test_score_df['loss'] = test_mae_loss
-    test_score_df['threshold'] = THRESHOLD
-    test_score_df['anomaly'] = test_score_df.loss > test_score_df.threshold
-    test_score_df['delay'] = test[TIME_STEPS:].Delay
-
-    anomalies = test_score_df[test_score_df.anomaly == True]
-    
+    pred = model.predict(df2)
     #pred = pred.where(pred==-1)
     #prediction = predict_image(model, image, inverted)
     #prediction = {'anomalies': where(pred == -1)}
-    prediction = anomalies['Delay'].tolist()
+    prediction = pred.tolist()
     # The following line allows Valohai to track endpoint predictions
     # while the model is deployed. Here we remove the full predictions
     # details as we are only interested in tracking the rest of the results.
--- train_model.py
+++ train_model.py
@@ -7,10 +7,6 @@ import tensorflow as tf
 import valohai
 import pandas as pd
 from sklearn.neighbors import LocalOutlierFactor
-
-from tensorflow import keras
-from sklearn.preprocessing import StandardScaler
-
 # def log_metadata(epoch, logs):
 #     """Helper function to log training metrics"""
 #     with valohai.logger() as logger:
@@ -18,63 +14,28 @@ from sklearn.preprocessing import StandardScaler
 #         logger.log('accuracy', logs['accuracy'])
 #         logger.log('loss', logs['loss'])
 
-def create_dataset(X, y, time_steps=1):
-    Xs, ys = [], []
-    for i in range(len(X) - time_steps):
-        v = X.iloc[i:(i + time_steps)].values
-        Xs.append(v)        
-        ys.append(y.iloc[i + time_steps])
-    return np.array(Xs), np.array(ys)
+
 def main():
+    # valohai.prepare enables us to update the valohai.yaml configuration file with
+    # the Valohai command-line client by running `valohai yaml step train_model.py`
 
     valohai.prepare(
         step='train-model',
         image='tensorflow/tensorflow:2.6.0',
         default_inputs={
-            'dataset': 'https://drive.google.com/file/d/1grwqu7MHB5CpiV9LmS9U8T53RQmV5Yqf/view?usp=sharing',
+            'dataset': 'http://www.testifytech.ml/Traffic_train.csv',
         }
     )
 
-    RANDOM_SEED = 42
-
-    np.random.seed(RANDOM_SEED)
-    tf.random.set_seed(RANDOM_SEED)
     # Read input files from Valohai inputs directory
     # This enables Valohai to version your training data
     # and cache the data for quick experimentation
     #print(valohai.inputs('dataset'))
-    df = pd.read_csv(valohai.inputs('dataset').path(), parse_dates=['Time'], index_col='Time')
-    train_size = int(len(df) * 0.95)
-    test_size = len(df) - train_size
-    train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
-    
-
-    scaler = StandardScaler()
-    scaler = scaler.fit(train[['Delay']])
-
-    train['Delay'] = scaler.transform(train[['Delay']])
-    test['Delay'] = scaler.transform(test[['Delay']])
-    TIME_STEPS = 30
-
-    # reshape to [samples, time_steps, n_features]
-
-    X_train, y_train = create_dataset(train[['Delay']], train.Delay, TIME_STEPS)
-    X_test, y_test = create_dataset(test[['Delay']], test.Delay, TIME_STEPS)
-
-    model = keras.Sequential()
-    model.add(keras.layers.LSTM(units=64, input_shape=(X_train.shape[1], X_train.shape[2])))
-    model.add(keras.layers.Dropout(rate=0.2))
-    model.add(keras.layers.RepeatVector(n=X_train.shape[1]))
-    model.add(keras.layers.LSTM(units=64, return_sequences=True))
-    model.add(keras.layers.Dropout(rate=0.2))
-    model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=X_train.shape[2])))
-    model.compile(loss='mae', optimizer='adam')
-
-    model.fit(X_train, y_train,epochs=10,batch_size=32,validation_split=0.1,shuffle=False)
-
-
-
-
+    data = pd.read_csv(valohai.inputs('dataset').path())
+    one_hot_encoded_data = pd.get_dummies(data, columns = ['Code'])
+    df = one_hot_encoded_data[["Delay", "Code_200", "Code_201", "Code_204", "Code_302", "Code_400", "Code_404", "Code_500"]]
+    model = LocalOutlierFactor(n_neighbors=20, novelty=True, contamination=0.1)
+    model.fit(df)
     #input_path = valohai.inputs('dataset').path()
     #with np.load(input_path, allow_pickle=True) as f:
     #    x_train, y_train = f['x_train'], f['y_train']
