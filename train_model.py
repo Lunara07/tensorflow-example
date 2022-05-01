4559e60a148c1ed463534d90d8b33f673ff18cf9
Lunara07 <49828654+Lunara07@users.noreply.github.com>
2022-05-02 00:30:24 +0300

    s

--- train_model.py
+++ train_model.py
@@ -25,13 +25,7 @@ from imblearn.over_sampling import SMOTE
 #         logger.log('accuracy', logs['accuracy'])
 #         logger.log('loss', logs['loss'])
 
-def create_dataset(X, y, time_steps=1):
-    Xs, ys = [], []
-    for i in range(len(X) - time_steps):
-        v = X.iloc[i:(i + time_steps)].values
-        Xs.append(v)        
-        ys.append(y.iloc[i + time_steps])
-    return np.array(Xs), np.array(ys)
+
 def main():
 
     valohai.prepare(
